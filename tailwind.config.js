20a7b04a4a63209a277dbccf3749f631de6c026f
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2026-08-16 18:16:51 +0300

    Update branding from Leyu to Mahder

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       colors: {
         primaryButton: "#095FAF",
         green: {
-          600: "#16a34a", // For the Leyu logo
+          600: "#16a34a", // For the Mahder logo
         },
         myGreen: "#00AC26",
         blue: {
